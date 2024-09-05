52280391e5fae58969cf6e217c17e33a8a71c5ad
dzakimaulana <dzakimaulanapati@gmail.com>
2024-09-05 22:16:19 +0700

    Create backend file structure

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const imageRoutes = require('./routes/imageRoutes');
+const app = express();
+
+app.use(express.json());
+app.use('/api/images', imageRoutes);
+
+module.exports = app;
